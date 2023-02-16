98c09a05b262afbdb0fd54a07259c451e9c39b7a
Winifred <winifredasmah246@gmail.com>
2023-02-16 12:33:11 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 
 
 /**
-  * main - prints "Programming is like building a multilingual puzzle followed by a new line"
-  * return 0.
+  * main - prints "Programming is like building a multilingual puzzle followed by a new line
+  *  Return Always 0.
   */
 
 int main (void)
@@ -11,5 +11,5 @@ int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	
-	return(0);
+	return (0);
 }
