3665019255b5b49e5dfef49d1fd2ed6dcf0a6a66
Winifred <winifredasmah246@gmail.com>
2023-02-17 13:45:41 -0800

    0-positive_or_negative.c

--- alx-low_level_programming/0x01-variables_if_else_while/0-positive_or_negative.c
+++ alx-low_level_programming/0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,33 +1,34 @@
 #include <stdio.h>
 #include <time.h>
-#include <stdio.h>
-
+#include <stdio.h.>
 /**
- * main -prints a random and states if 
- * its a positive ,negative or zero.
- * Return: 0 if its false and non-zero otherwise 
+ * main - prints a random and sttates if
+ * its a positive, negative or zero.
+ * Return: o if its true or non-zero otherwise
  * This program will assign a random
- * number to the variable  n each time it is executed.
- * complete the source code in order to print
- * whether the number stored in the  variable n is positive  or negative 
+ * number to the variable n each time it is executed.
+ * Complete the source code in order to print
+ * whether the number stored in the variable n is positive or negative.
  */
-
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there*/
-if (n > 0)
-{
-printf("%d is positive\n", n);
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+	return (0);
 }
-else if (n < 0)
-{
-printf("%d is negative\n", n);
-}
-else  
-{
-printf("%d is zero\n", n)
-}
-return (0);
+
