e66387f8939af7fba9dc364d57ba30f50a0151ee
Winifred <winifredasmah246@gmail.com>
2023-02-17 17:05:58 -0800

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include < stdio.h >
+/**
+*main - prints alphabets
+*Return: 0
+*Description - All your code should be in the main function
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+	putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
