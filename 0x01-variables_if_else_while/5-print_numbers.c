14c89ce64fca0a68db05bbf1d81af1a2d8ec8f79
Winifred <winifredasmah246@gmail.com>
2023-02-17 18:24:46 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints numbers from zero to nine
+ * Return: 0
+ * Description - All your code should be in the main function
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
