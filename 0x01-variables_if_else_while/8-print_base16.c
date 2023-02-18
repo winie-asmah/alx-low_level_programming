5141d37a16c661ea9b2ad1c35797aa26f250c7b0
Winifred <winifredasmah246@gmail.com>
2023-02-17 18:46:54 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - prints numbers of base 16
+ * Return: 0
+ * Description - All your code should be in the main function
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
