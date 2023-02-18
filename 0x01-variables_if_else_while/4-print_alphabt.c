3874cd40ac2728d5582675201dedfe626ed846c5
Winifred <winifredasmah246@gmail.com>
2023-02-17 18:06:47 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *  * main - prints alphabets excerpt q and e
- *   * Return: 0
- *    * Description - All your code should be in the main function
- *     */
+ * main - prints alphabets excerpt q and e
+ * Return: 0
+ * Description - All your code should be in the main function
+ */
 int main(void)
 {
 	char ch;
