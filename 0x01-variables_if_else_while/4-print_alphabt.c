5648bc8b2d225a0fa54367548929889c3b8cca47
Winifred <winifredasmah246@gmail.com>
2023-02-17 18:04:50 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *  * main - prints alphabets excerpt q and e
+ *   * Return: 0
+ *    * Description - All your code should be in the main function
+ *     */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
