8599f6d26281a8b3735a94724766baf68981df6f
Winifred <winifredasmah246@gmail.com>
2023-02-17 16:45:46 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,33 +1,34 @@
 #include <stdlib.h>
 #include <time.h>
-
+#include <stdio.h>
 /**
  * main - print the last digit of the number
  * stored in the variable n
  * Return: 0 id executed successfully
  * or non-zero ortherwise
- */	
+ */
 int main(void)
 {
 	int n;
-	int m;
+	int a;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	m = n % 10;
+	a = n % 10;
 	if (m > 5)
-	{		
-		printf("Last digit of %d is %d and is greater than 5\n", n, m);
+	{	
+		printf("Last digit of %d is %d and is greater than 5\n", n, a);
 	}
-	else if (m == 0)
-	{
-		printf("Last digit of %d is %d and is 0\n", n, m);
-	}
+	else if (a == 0)
+	{	
+		printf("Last digit of %d is %d and is 0\n", n, a);
+	}	
+	
 
 
 	else
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
+	{	
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 	}
 	return (0);
 }	
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include < stdio.h >
+/**
+ *main - prints the alphabet
+ *Return: 0
+ *Description - All your code should be in the main function
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch < = 'z'; ch++)
+	{
+		putchar(ch);
+	{							
+	putchar('\n');
+	return (0);
+}
