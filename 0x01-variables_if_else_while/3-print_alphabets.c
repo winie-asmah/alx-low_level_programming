474248df8cb54214532a2079a3d6224d145dcfb0
Winifred <winifredasmah246@gmail.com>
2023-02-17 17:53:37 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,16 +6,15 @@
 */
 int main(void)
 {
-	char ch;
+	char c;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		putchar(ch);
-	}
-	for (ch = 'A'; ch <= 'Z'; ch++)
-	{
-	putchar(ch);
-	}
+	c = 'a';
+	while 
+		(c <= 'z'; ) {	
+			if((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++
+		}
 	putchar('\n');
 	return (0);
 }
