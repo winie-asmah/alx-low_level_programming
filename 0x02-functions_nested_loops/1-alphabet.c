d63b73f4bbb0ddaa9779859451fd04f5381a1c7c
Winifred <winifredasmah246@gmail.com>
2023-02-21 08:21:00 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *print_alphabet - prints alphabets from a-z
+ * Return: 0 when successful
+ * Description - You can only use _putchar twice in your code
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a';  ch <= 'z'; ch++)
+	{
+		_puchar(ch);
+	}
+	
+	_putchar('\n');
+
+
+
+
+
+
+}
+
