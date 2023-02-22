9d46216b646ca7d8996b4a0ee5ea0fd176653246
Winifred <winifredasmah246@gmail.com>
2023-02-22 08:15:45 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,17 +6,22 @@
  * @n: Starting number
  * Return: void
  */
-void prin_to_98(int n)
+void print_to_98(int n)
 {
 	if (n < 98)
 	{
 		for (; n < 98; n++)
+		{
+
 			printf("%d,", n);
+		}
 	}
 	else 
 	{
 		for (; n < 98; n++);
+		{
 			printf("%d,", n);
+		}
 
 	}
 	printf("%d\n", 98);
