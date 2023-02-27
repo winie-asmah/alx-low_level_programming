4f5d87a2084e2c7e6e0d199756d992d10f97f8f1
Winifred <winifredasmah246@gmail.com>
2023-02-27 07:40:48 -0800

    0-reset_to_98.c

--- alx-low_level_programming/0x05-pointers_arrays_strings/0-reset_to_98.c
+++ alx-low_level_programming/0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * rest_to_98 - takes a pointer to an int as parameter
- *  and updates the value it prints to 98
- * @n: parameter to take the value
+ * reset_to_98 - takes a pointer to an int as parameter
+ * and updates the value it prints to 98
+ * @n: parameter to take the value.
+ *
  */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
