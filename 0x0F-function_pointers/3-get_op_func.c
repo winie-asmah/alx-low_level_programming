d53c37de9419ce82318a4cc786819f24a041e649
Winifred <winifredasmah246@gmail.com>
2023-03-24 03:15:41 -0700

    function pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,57 +1,33 @@
+#include "3-calc.h"
+#include <stdlib.h>
 /**
- * op_add - Adds two numbers
- * @a: int 1
- * @b: int 2
- * Return: Returns result
-**/
-int op_add(int a, int b)
+ * get_op_func - Selects the correct function to perform
+*               the operation asked by the user.
+* @s: The operator passed as argument.
+*
+* Return: A pointer to the function corresponding
+*         to the operator given as a parameter.
+*/
+int (*get_op_func(char *s))(int, int)
 {
-return (a + b);
+	op_t ops[] =
+       	{
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL},
+	};
+
+
+	int i = 0;
+
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+			i++;
+
+
+	return (ops[i].f);
 }
 
-/**
- * op_sub - subtracts two numbers
- * @a: int 1
- * @b: int 2
- * Return: Returns result
-**/
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
 
-/**
- * op_mul - Multiplies two numbers
- * @a: int 1
- * @b: int 2
- * Return: Returns result
-**/
-
-int op_mul(int a, int b)
-{
-return (a * b);
-}
-
-/**
- * op_div - Divides two numbers
- * @a: int 1
- * @b: int 2
- * Return: Returns result
-**/
-
-int op_div(int a, int b)
-{
-	return (a / b);
-}
-
-/**
- * op_mod - finds Modules two numbers
- * @a: int 1
- * @b: int 2
- * Return: Returns result
-**/
-
-int op_mod(int a, int b)
-{
-return (a % b);
-}
